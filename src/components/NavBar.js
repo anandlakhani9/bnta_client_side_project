4126bd39724b114ca88aa714773098565f920ce3
nagiinaaa <nagina.r@hotmail.co.uk>
2021-11-30 16:42:38 +0000

    added dropdown menu to navbar and changed styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,74 @@
 import '../styles/NavBar.css'
 import ThemeToggle from './ThemeToggle';
+import {useState} from "react"
 
 
-const NavBar = ({searchChange}) => {
+const NavBar = ({goHome, searchChange, filteredToBreakFast, filteredToLunch, filteredToDinner, filteredToSnack, 
+    filteredToDessert, filteredToVegetarian, filteredToVegan, filteredToMeatOnly, filteredToPescatarian,
+    filteredToHot, filteredToMedium, filteredToMild, filteredToSweet, filteredTo30mins, filteredToHour,
+    filteredToOverHour }) => {
+
+        const [dropdown, setDropdown] = useState(false);
+
+        const dropdownClick = () => {
+
+            if(dropdown == false){
+                setDropdown(true);
+            } else {
+                setDropdown(false);
+            }
+        }
 
     return (
-        <div className="navBar">
-            <h1>Day Owls</h1>
+        <div>
+            <div className="navBar">
+            <h1 onClick={goHome}>Day Owls</h1>
             <input className="searchBar" type="search" placeholder="search..."  onChange = {searchChange}/>
-            <button>Allergens</button>
+            <button onClick={dropdownClick}>Menu</button>
             <ThemeToggle />
+            </div>
+
+            {dropdown == true ?
+            (<div className="menuLinks">
+            <div className="mealTypeMenu">
+                <p>Meal Type</p>
+                <div className="mealTypeLinks">
+                    <html onClick={filteredToBreakFast}>Breakfast</html>
+                    <html onClick={filteredToLunch}>Lunch</html>
+                    <html onClick={filteredToDinner}>Dinner</html>
+                    <html onClick={filteredToSnack}>Snack</html>
+                    <html onClick={filteredToDessert}>Dessert</html>
+                    </div>
+                </div>
+                <div className="dietsMenu">
+                <p>Diets</p>
+                <div className="dietsLinks">
+                    <html onClick={filteredToVegetarian}>Vegetarian</html>
+                    <html onClick={filteredToVegan}>Vegan</html>
+                    <html onClick={filteredToMeatOnly}>Meat only</html>
+                    <html onClick={filteredToPescatarian}>Pescatarian</html>
+                </div>
+                </div>
+                <div className="spiceRatingMenu">
+                <p>Spice Rating</p>
+                <div className="spiceRatingLinks">
+                    <html onClick={filteredToHot}>Hot</html>
+                    <html onClick={filteredToMedium}>Medium</html>
+                    <html onClick={filteredToMild}>Mild</html>
+                    <html onClick={filteredToSweet}>Sweet</html>
+                    </div>
+                </div>
+                <div className="cookingTimeMenu">
+                <p>Cooking Time</p>
+                <div className="cookingTimeLinks">
+                    <html onClick={filteredTo30mins}>Under 30 mins</html>
+                    <html onClick={filteredToHour}>Up to an hour</html>
+                    <html onClick={filteredToOverHour}>Over an hour</html>
+                    </div>
+                </div>
+            </div>)
+            :
+            (null)}
         </div>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,7 @@ import NavBar from "../components/NavBar"
 const HomePage = () => {
     const [recipes, setRecipes] = useState(null);
     const [input, setInput] = useState("");
+    const [recipesFiltered, setRecipesFiltered] = useState([]);
 
     useEffect(()=>{
         getAllRecipes().then(response => setRecipes(response.filter((recipes)=>{
@@ -17,28 +18,142 @@ const HomePage = () => {
     const searchChange = (s) => {
         console.log(s.target.value)
         setInput(s.target.value)
+        setRecipesFiltered("");
     };
 
 
     const filteredRecipes = 
     recipes ?
-        recipes.filter((recipes)=>{
+        recipes.filter((recipes)=> {
             return recipes.name.toLowerCase().includes(input.toLowerCase());
         })
         :
         null;
+
+        const filteredToBreakFast = () => { 
+   
+            const newRecipes = recipes.filter((recipes)=> { return recipes.mealType.toLowerCase().includes("breakfast") })
+            setRecipesFiltered(newRecipes);
+        }
+    
+        const filteredToLunch = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.mealType.toLowerCase().includes("lunch") })
+            setRecipesFiltered(newRecipes);
+        }
+    
+        const filteredToDinner = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.mealType.toLowerCase().includes("dinner") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToSnack = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.mealType.toLowerCase().includes("snack") })
+            setRecipesFiltered(newRecipes);
+        }
+    
+        const filteredToDessert = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.mealType.toLowerCase().includes("dessert") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToVegetarian = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.vegetarian })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToVegan = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.vegan })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToMeatOnly = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.meatOnly })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToPescatarian = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.pescatarian })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToHot = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.spiceRating.toLowerCase().includes("hot") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToMedium = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.spiceRating.toLowerCase().includes("medium") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToMild = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.spiceRating.toLowerCase().includes("mild") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToSweet = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.spiceRating.toLowerCase().includes("sweet") })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredTo30mins = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.cookingTime < 31 })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToHour = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.cookingTime < 61 })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const filteredToOverHour = () => { 
+       
+            const newRecipes = recipes.filter((recipes)=> { return recipes.cookingTime > 61 })
+            setRecipesFiltered(newRecipes);
+        }
+
+        const goHome = () => {
+            setRecipesFiltered("");
+        }
+
+        const recipesToShow = recipesFiltered.length > 0 ? recipesFiltered : filteredRecipes;  
         
 
     return (
         recipes ?
         <>
-            <NavBar searchChange={searchChange} value={input} />
-            <CardList recipes={filteredRecipes} />
+
+            <NavBar goHome={goHome} searchChange={searchChange} value={input} filteredToBreakFast={filteredToBreakFast} 
+            filteredToLunch={filteredToLunch} filteredToDinner={filteredToDinner} filteredToSnack={filteredToSnack} 
+            filteredToDessert={filteredToDessert} filteredToVegetarian={filteredToVegetarian} filteredToVegan={filteredToVegan} 
+            filteredToMeatOnly={filteredToMeatOnly} filteredToPescatarian={filteredToPescatarian} filteredToHot={filteredToHot}
+            filteredToMedium={filteredToMedium} filteredToMild={filteredToMild} filteredToSweet={filteredToSweet}
+            filteredTo30mins={filteredTo30mins} filteredToHour={filteredToHour} filteredToOverHour={filteredToOverHour}/>
+            <CardList recipes={recipesToShow} />
         </>
 
         :
         <>
-        <NavBar searchChange={searchChange} value={input}/>
+        <NavBar searchChange={searchChange} value={input} filteredToBreakFast={filteredToBreakFast} 
+            filteredToLunch={filteredToLunch} filteredToDinner={filteredToDinner} filteredToSnack={filteredToSnack} 
+            filteredToDessert={filteredToDessert} filteredToVegetarian={filteredToVegetarian} filteredToVegan={filteredToVegan} 
+            filteredToMeatOnly={filteredToMeatOnly} filteredToPescatarian={filteredToPescatarian} filteredToHot={filteredToHot}
+            filteredToMedium={filteredToMedium} filteredToMild={filteredToMild} filteredToSweet={filteredToSweet}
+            filteredTo30mins={filteredTo30mins} filteredToHour={filteredToHour} filteredToOverHour={filteredToOverHour}/>
         <p>Loading...</p>
         </>
     )
