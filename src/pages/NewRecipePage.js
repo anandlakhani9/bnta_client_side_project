9c3d1c356d5e5bd4712d98dae1447dfad88af2a5
mechanin <86329519+mechanin@users.noreply.github.com>
2021-11-30 16:13:58 +0000

    COMPLETED: Add New Recipe Feature
    Co-Authored-By: Jess Morse <90448325+JessMorse@users.noreply.github.com>

--- src/pages/NewRecipePage.js
+++ src/pages/NewRecipePage.js
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import RecipeForm from "../components/RecipeForm";
 
 const NewRecipePage = () => {
     return(
+        <>
         <p>This is the New Recipes Page</p>
+        <RecipeForm />
+        </>
     )
 
 }
