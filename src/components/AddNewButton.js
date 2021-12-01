d492bf45267e0bc9e1da40b606f5f9ca2d7fb1a9
Jessica Morse <jess0morse99@gmail.com>
2021-12-01 12:29:56 +0000

    added some styling to add-new buttons, need to fix positioning

--- src/components/AddNewButton.js
+++ src/components/AddNewButton.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import "../styles/AddNewButton.css"
 
 const AddNewButton = (props) => {
 
@@ -22,16 +23,22 @@ const AddNewButton = (props) => {
     }
 
     return(
+        
         AddNewButtonState === "closed" ?
-        <>
-            <button onClick={handleClick}>+</button>
-        </>
+        <div className="addButton">
+            <button onClick={handleClick} className="addNew">+</button>
+            </div>
         :
-            <>
-            <button onClick={handleClick}>-</button>
-            <button onClick = {goNewRecipePage}>Add New Recipe</button>
-            <button onClick = {goNewIngredientPage}>Add New Ingredient</button>
-            </>
+        <div className="addButton">
+            
+            
+            <button onClick = {goNewRecipePage} className="add addRecipe">Add New Recipe</button>
+            <button onClick = {goNewIngredientPage} className="add addIngredient">Add New Ingredient</button>
+            <button onClick={handleClick} className="addNew">-</button>
+
+            </div>
+
+      
     )
 }
 
