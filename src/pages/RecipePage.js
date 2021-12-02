6df260810717dcf7298236bb75932ca53e740358
anandlakhani9 <58656737+anandlakhani9@users.noreply.github.com>
2021-12-02 12:25:01 +0000

    added recipe to recipe page

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,15 +1,30 @@
-import { useEffect, useState } from "react/cjs/react.development"
 import { getRecipeById } from "../adapters/recipeAdapter"
 import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 
-const RecipePage = () => {
-    // const [recipe, setRecipe] = useState(null)
-    // useEffect(()=>{
-    //     getRecipeById(id)
-    // })
+
+const RecipePage = (props) => {
+
     const { id } = useParams();
+    const [recipe, setRecipe] = useState(null)
+    useEffect(()=> {
+        getRecipeById(id).then(r => setRecipe(r))
+    })
     return(
-        <p>{id}</p>
+        recipe ?
+        <div className="recipe-page">
+        <h1>{recipe.name}</h1>
+        <h3>{`Cuisine: ${recipe.cuisine}`}</h3>
+        <h3>{`Spice Level: ${recipe.spiceRating}`}</h3>
+        <h3>{`Cooking Time: ${recipe.cookingTime} minutes`}</h3>
+        <h3>{`Meal Type: ${recipe.mealType}`}</h3>
+        <div className="cooking-instructions">
+            <h2>Instructions</h2>
+            <p><span className="recipe-instructions">{recipe.instructions}</span></p>
+        </div>
+        </div>
+        :
+        <p>Loading...</p>
     )
 }
 
