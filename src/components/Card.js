ac78051e848631e0fb71ce2428ef3ef9929cc01f
mechanin <86329519+mechanin@users.noreply.github.com>
2021-12-02 17:45:22 +0000

    Added Recipe Image Placeholders

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,55 +1,64 @@
-import '../styles/Card.css'
+import "../styles/Card.css";
 import React, { useState, useEffect, useContext } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { AiOutlineStar } from "react-icons/ai";
 import { ThemeContext } from "../contexts/ThemeContext";
-import { useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import food_placeholder from "../images/food_placeholder.png"
 
-const Card = ({name,key,id,isVegan,isVegetarian,isMeatOnly,isPescatarian, addToFaves, removeFromFaves, favourites}) => {
-    const darkTheme = useContext(ThemeContext);
-    const darkMode = darkTheme.state.darkMode;
+const Card = ({
+  name,
+  id,
+  isVegan,
+  isVegetarian,
+  isMeatOnly,
+  isPescatarian,
+  addToFaves,
+  removeFromFaves,
+  favourites,
+}) => {
+  const darkTheme = useContext(ThemeContext);
+  const darkMode = darkTheme.state.darkMode;
 
-    const[theme, setTheme] = useState("");
+  const [theme, setTheme] = useState("");
 
-    useEffect(() => {
-        if(isVegan) {
-            //console.log("vegan")
-            setTheme("card-vegan")
-        }
-        else if (isVegetarian){
-            setTheme("card-vegetarian")
-        }
-        else if (isPescatarian){
-            setTheme("card-pesc")
-        }
-        else if (isMeatOnly){
-            setTheme("card-meat")
-        }
-        }, [isVegan, isVegetarian, isMeatOnly, isPescatarian]);
-  
-        let navigate = useNavigate();
+  useEffect(() => {
+    if (isVegan) {
+      //console.log("vegan")
+      setTheme("card-vegan");
+    } else if (isVegetarian) {
+      setTheme("card-vegetarian");
+    } else if (isPescatarian) {
+      setTheme("card-pesc");
+    } else if (isMeatOnly) {
+      setTheme("card-meat");
+    }
+  }, [isVegan, isVegetarian, isMeatOnly, isPescatarian]);
 
+  let navigate = useNavigate();
 
-    
-    return (
-        <div className={`card ${theme} ${darkMode ? "card-dark" : "card-light"}`}>
-       
-
-            {favourites == true ?
-            (<button onClick={() => removeFromFaves(id)}><AiFillStar /></button>)
-            :(<button onClick={() => addToFaves(id)}><AiOutlineStar /></button>)}
-            <Link to={{pathname: `/recipe/${id}` }}>
-            <div className="link-to-recipe">
-            {/* <img src={props.image} alt={props.alt}/> */}
-            <p>{name}</p>
-            <p>Some description</p>
-            </div>
-            </Link>
+  return (
+    <div className={`card ${theme} ${darkMode ? "card-dark" : "card-light"}`}>
+      {favourites == true ? (
+        <button onClick={() => removeFromFaves(id)}>
+          <AiFillStar />
+        </button>
+      ) : (
+        <button onClick={() => addToFaves(id)}>
+          <AiOutlineStar />
+        </button>
+      )}
+      <Link to={{ pathname: `/recipe/${id}` }}>
+        <div className="link-to-recipe">
+          {/* <img src={props.image} alt={props.alt}/> */}
+          <img className ="food-image" src={food_placeholder} alt="placeholder"/>
+          <p>{name}</p>
+          <p>Some description</p>
         </div>
-        
-
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
 export default Card;
